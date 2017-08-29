9db4fa7a2f6557e0ff2ff4d7efc44daca470c4f2
Rubén Antón <rubocoptero@gmail.com>
2017-08-29 22:27:38 +0200

    Proxies BetterDoctor API response

--- app/server.js
+++ app/server.js
@@ -1,9 +1,26 @@
-var express = require('express')
+var express = require('express');
 var app = express();
+var request = require('request');
 
 app.get('/api/v1/doctors/search', function (req, res) {
-  res.sendStatus(200);
+  endpoint = 'https://api.betterdoctor.com/2016-03-01/doctors?name=Ruben'
+  request.get(endpoint, function (error, response, body) {
+    res.send(body);
+  });
 });
 
-app.listen(3000);
-console.log('Server running at http://127.0.0.1:3000/');
+var server = null;
+
+if (process.env.NODE_ENV !== 'test') {
+  server = app.listen(3000);
+  console.log('Server running at http://127.0.0.1:3000/');
+}
+
+exports.listen = function (port) {
+  server = app.listen(port);
+  console.log('Server running at http://127.0.0.1:' + port + '/');
+};
+
+exports.stop = function () {
+  server.close();
+};
--- app/spec/proxy.spec.js
+++ app/spec/proxy.spec.js
@@ -1,10 +1,38 @@
+process.env.NODE_ENV = 'test'
+
 var request = require('request');
+var nock = require('nock');
+var app = require('../server.js');
 
 describe('Proxy spec', function () {
+  var ENDPOINT = 'http://localhost:3001/api/v1/doctors/search?name=Ruben'
+  var betterDoctorResponse = { response: 'WHATEVER' };
+
+  beforeAll(function () {
+    app.listen(3001);
+  });
+
+  afterAll(function () {
+    app.stop();
+  });
+
+  beforeEach(function () {
+    nock('https://api.betterdoctor.com')
+      .get('/2016-03-01/doctors?name=Ruben')
+      .reply(200, betterDoctorResponse);
+  });
+
 	it('has a search doctors endpoint', function (done) {
-		request.get('http://localhost:3000/api/v1/doctors/search?name=John', function (error, response, body) {
+		request.get(ENDPOINT, function (error, response, body) {
 		  expect(response.statusCode).not.toEqual(404);
 		  done();
 		});
 	});
+
+  it('proxies BetterDoctor API response', function (done) {
+    request.get(ENDPOINT, function (error, response, body) {
+      expect(body).toEqual(JSON.stringify(betterDoctorResponse));
+      done();
+    });
+  });
 });
