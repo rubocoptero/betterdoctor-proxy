6db52db5317f688bdccfdb272f7f8cc196479b87
Rubén Antón <rubocoptero@gmail.com>
2017-08-30 13:42:56 +0200

    Factorize search request

--- app/src/services/better-doctor.js
+++ app/src/services/better-doctor.js
@@ -1,10 +1,16 @@
 var request = require('request-promise');
 var userKey = process.env.BETTER_DOCTOR_USER_KEY;
+var baseUrl = 'https://api.betterdoctor.com';
 
 var searchBy = function (name) {
-  var endpoint = 'https://api.betterdoctor.com/2016-03-01/doctors?name=' + name + '&user_key=' + userKey;
-
-  return request.get(endpoint);
+  return request.get({
+    baseUrl: baseUrl,
+    uri: '/2016-03-01/doctors',
+    qs: {
+      user_key: userKey,
+      name: name
+    }
+  });
 };
 
 module.exports = { searchBy: searchBy };
